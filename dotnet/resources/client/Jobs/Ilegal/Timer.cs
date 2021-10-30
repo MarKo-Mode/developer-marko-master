6ba2072595cb83465fce58ffa799035ddf669cbe
MarKo-Mode <93425696+MarKo-Mode@users.noreply.github.com>
2021-10-30 11:45:08 +0300

    RAGEMP 1.1 V0.0.1
    RAGEMP 1.1  V0.0.1

---
+++ dotnet/resources/client/Jobs/Ilegal/Timer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using GTANetworkAPI;
+
+public class TimerEx : Script
+{
+
+    private static readonly List<TimerEx> timer = new List<TimerEx>();
+    private static List<TimerEx> insertAfterList = new List<TimerEx>();
+    private static Stopwatch stopwatch = new Stopwatch();
+    public Action Func;
+    public readonly uint ExecuteAfterMs;
+    private ulong executeAtMs;
+    public uint ExecutesLeft;
+    public bool HandleException;
+    private bool willRemoved = false;
+    public bool IsRunning
+    {
+        get
+        {
+            return !willRemoved;
+        }
+    }
+
+    public TimerEx()
+    {
+        stopwatch.Start();
+    }
+
+    private TimerEx(Action thefunc, uint executeafterms, ulong executeatms, uint executes, bool handleexception)
+    {
+        Func = thefunc;
+        ExecuteAfterMs = executeafterms;
+        executeAtMs = executeatms;
+        ExecutesLeft = executes;
+        HandleException = handleexception;
+    }
+
+    public static TimerEx SetTimer(Action thefunc, uint executeafterms, uint executes = 1, bool handleexception = false)
+    {
+        ulong executeatms = executeafterms + GetTick();
+        TimerEx thetimer = new TimerEx(thefunc, executeafterms, executeatms, executes, handleexception);
+        insertAfterList.Add(thetimer);
+        return thetimer;
+    }
+
+    private static ulong GetTick()
+    {
+        return (ulong)stopwatch.ElapsedMilliseconds;
+    }
+
+    public void Kill()
+    {
+        willRemoved = true;
+    }
+
+    private void ExecuteMe()
+    {
+        Func();
+        if (ExecutesLeft == 1)
+        {
+            ExecutesLeft = 0;
+            willRemoved = true;
+        }
+        else
+        {
+            if (ExecutesLeft != 0)
+                ExecutesLeft--;
+            executeAtMs += ExecuteAfterMs;
+            insertAfterList.Add(this);
+        }
+    }
+
+    private void ExecuteMeSafe()
+    {
+        try
+        {
+            Func();
+        }
+        catch (Exception ex)
+        {
+            NAPI.Util.ConsoleOutput(ex.ToString());
+        }
+        finally
+        {
+            if (ExecutesLeft == 1)
+            {
+                ExecutesLeft = 0;
+                willRemoved = true;
+            }
+            else
+            {
+                if (ExecutesLeft != 0)
+                    ExecutesLeft--;
+                executeAtMs += ExecuteAfterMs;
+                insertAfterList.Add(this);
+            }
+        }
+    }
+
+    public void Execute(bool changeexecutems = true)
+    {
+        if (changeexecutems)
+        {
+            executeAtMs = GetTick();
+        }
+        if (HandleException)
+            ExecuteMeSafe();
+        else
+            ExecuteMe();
+    }
+
+    private void InsertSorted()
+    {
+        bool putin = false;
+        for (int i = timer.Count - 1; i >= 0 && !putin; i--)
+            if (executeAtMs <= timer[i].executeAtMs)
+            {
+                timer.Insert(i + 1, this);
+                putin = true;
+            }
+
+        if (!putin)
+            timer.Insert(0, this);
+    }
+
+    [ServerEvent(Event.Update)]
+    public static void OnUpdateFunc()
+    {
+        ulong tick = GetTick();
+        for (int i = timer.Count - 1; i >= 0; i--)
+        {
+            if (!timer[i].willRemoved)
+            {
+                if (timer[i].executeAtMs <= tick)
+                {
+                    TimerEx thetimer = timer[i];
+                    timer.RemoveAt(i);
+                    if (thetimer.HandleException)
+                        thetimer.ExecuteMeSafe();
+                    else
+                        thetimer.ExecuteMe();
+                }
+                else
+                    break;
+            }
+            else
+                timer.RemoveAt(i);
+        }
+
+        if (insertAfterList.Count > 0)
+        {
+            foreach (TimerEx timer in insertAfterList)
+            {
+                timer.InsertSorted();
+            }
+            insertAfterList.Clear();
+        }
+    }
+}
---
+++ dotnet/resources/client/Settings/Database.cs
@@ -0,0 +1,113 @@
+﻿using MySqlConnector;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Redage.SDK
+{
+    public static class Database
+    {
+        private static readonly Config config = new Config("MySQL");
+        private static readonly nLog Log = new nLog("MySQL");
+
+        private static string Connection;
+        public static bool Debug = false;
+        public static void Init()
+        {
+            if (Connection is string) return;
+            Connection =
+                $"Host={config.TryGet<string>("Server", "localhost")};" +
+                $"Port={config.TryGet<string>("Port", 3306)};" +
+                $"User={config.TryGet<string>("User", "root")};" +
+                $"Password={config.TryGet<string>("Password", "")};" +
+                $"Database={config.TryGet<string>("DataBase", "marko")};" +
+                $"{config.TryGet<string>("SSL", "SslMode=None;")}";
+        }
+
+        public static void Query(MySqlCommand command)
+        {
+            try
+            {
+                if (Debug) Log.Debug("Query to DB:\n" + command.CommandText);
+                using MySqlConnection connection = new MySqlConnection(Connection);
+                connection.Open();
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e) { Log.Write(e.ToString(), nLog.Type.Error); }
+        }
+
+        public static void Query(string command)
+        {
+            using MySqlCommand cmd = new MySqlCommand(command);
+            Query(cmd);
+        }
+
+        public static async Task QueryAsync(MySqlCommand command)
+        {
+            try
+            {
+                if (Debug) Log.Debug("Query to DB:\n" + command.CommandText);
+                using MySqlConnection connection = new MySqlConnection(Connection);
+                await connection.OpenAsync();
+                command.Connection = connection;
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception e) { Log.Write(e.ToString(), nLog.Type.Error); }
+        }
+        public static async Task QueryAsync(string command)
+        {
+            try
+            {
+                if (Debug) Log.Debug("Query to DB:\n" + command);
+                using MySqlConnection connection = new MySqlConnection(Connection);
+                await connection.OpenAsync();
+                using MySqlCommand cmd = new MySqlCommand
+                {
+                    Connection = connection,
+                    CommandText = command
+                };
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception e) { Log.Write(e.ToString(), nLog.Type.Error); }
+        }
+        public static DataTable QueryRead(MySqlCommand command)
+        {
+            if (Debug) Log.Debug("Query to DB:\n" + command.CommandText);
+            using MySqlConnection connection = new MySqlConnection(Connection);
+            connection.Open();
+            command.Connection = connection;
+            DbDataReader reader = command.ExecuteReader();
+            DataTable result = new DataTable();
+            result.Load(reader);
+
+            return result;
+        }
+        public static DataTable QueryRead(string command)
+        {
+            using MySqlCommand cmd = new MySqlCommand(command);
+            return QueryRead(cmd);
+        }
+        public static async Task<DataTable> QueryReadAsync(MySqlCommand command)
+        {
+            if (Debug) Log.Debug("Query to DB:\n" + command.CommandText);
+            using MySqlConnection connection = new MySqlConnection(Connection);
+            await connection.OpenAsync();
+            command.Connection = connection;
+            DbDataReader reader = await command.ExecuteReaderAsync();
+            DataTable result = new DataTable();
+            result.Load(reader);
+            return result;
+        }
+        public static async Task<DataTable> QueryReadAsync(string command)
+        {
+            using MySqlCommand cmd = new MySqlCommand(command);
+            return await QueryReadAsync(cmd);
+        }
+        public static string ConvertTime(DateTime DateTime)
+        {
+            return DateTime.ToString("s");
+        }
+    }
+}
